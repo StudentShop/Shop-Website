aa45f5e6c07297bd6d7ea7713c2e645f4d9d234d
Charles Allhands <charles.allhands@wisc.edu>
2015-08-10 14:46:15 -0500

    Creat woodworking 2 upgrade

---
+++ permit/info/upgrades/woodworking_2.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+	<!-- ****************************************************************************
+**********															*************
+**********		INSERT PAGE TITLE BELOW within quotations!!!! 		*************
+**********		Title should be the same as the <h1> tag below 		*************
+**********															*************
+*********************************************************************************	-->
+	<?php $subtitle = "Woodworking 2 Upgrade";?>
+
+
+		<?php require ($_SERVER['DOCUMENT_ROOT']."/functions.php"); ?>
+			<?php require ($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+				<?php require ($_SERVER['DOCUMENT_ROOT']."/main_nav.php"); ?>
+
+					<!-- ************************* 				End of Navigation Items 			****************************  -->
+
+					<div id="wrapper">
+						<!-- wrapper sets the content area including the left nav, middle (main content), and right nav  -->
+
+						<div id="leftNav">
+
+							<?php include ("../../permit_left_nav.php") ?>
+								<!-- Left Navigation in the include file -->
+
+								<h2 class="subnav2">
+									<span>Related Links</span>
+								</h2>
+								<ul>
+									<li><a href="/permit/info/upgrades/Woodworking_1.php">Woodworking 1 Upgrade</a></li>
+									<li><a href="/permit/info/upgrades/welding_1.php">Welding 1 Upgrade</a></li>
+								</ul>
+						</div>
+						<!-- END OF LEFT NAV DIV -->
+
+						<div id="MainContent">
+							<!-- sets the main content (middle column area) -->
+
+							<h1>Woodworking 2 Upgrade</h1>
+
+							<h2>Overview</h2>
+							<p>The woodworking 2 upgrade is an additional amount of training individuals can take to learn how to safely use a variety of woodworking equipment (<a href="https://en.wikipedia.org/wiki/Jointer"
+								rel="external">Jointer</a>, <a href="https://en.wikipedia.org/wiki/Thickness_planer" rel="external">Thickness planer</a>, <a href="https://en.wikipedia.org/wiki/Biscuit_joiner" rel="external">biscuit joiner</a>, and <a href="https://en.wikipedia.org/wiki/Router_%28woodworking%29"
+								rel="external">router</a>) in the Student Shop. Individuals will not be allowed to use this woodworking equipment until they have obtained the woodworking 2 upgrade.</p>
+
+							<h2>Privileges</h2>
+							<ul>
+								<li>Use the Shop jointer</li>
+								<li>Use the Shop thickness planer</li>
+								<li>Use the Shop biscuit joiner</li>
+								<li>Use Shop wood routers</li>
+							</ul>
+
+							<h2>Prerequisites</h2>
+							<ul>
+								<li>Red or green shop permit</li>
+								<li>
+									<a href="/permit/info/upgrades/woodworking_1.php">Woodworking 1 Upgrade</a></li>
+							</ul>
+
+							<h2>Requirements</h2>
+							<p>(must be completed in order)</p>
+							<ul>
+								<li><a href="https://emu.engr.wisc.edu/emu" rel="external">Apply for the Woodworking 2 Upgrade</a>&#42;</li>
+								<li>Enroll in and attend the <a href="/permit/info/seminars/woodworking_1.php">woodworking 2 seminar</a></li>
+							</ul>
+							<p>&#42;Individuals will only be able to see upgrades in EMU for which they have completed the prerequisites.</p>
+
+							<h2>Time Frame</h2>
+							<ul>
+								<li>Upgrades must be completed within 45 days of applying. Failure to meet the 45 day deadline will result in all progress towards the upgrade being removed</li>
+							</ul>
+							<br />
+
+						</div>
+						<!-- END OF MAINCONTENT -->
+
+					</div>
+					<!-- END OF WRAPPER -->
+
+					<?php require ($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
+
+						</body>
+
+</html>
