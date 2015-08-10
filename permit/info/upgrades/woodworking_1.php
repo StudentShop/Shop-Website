fcdc060e658a83bf272cc5ad38c0511c42e66481
Charles Allhands <charles.allhands@wisc.edu>
2015-08-10 14:45:23 -0500

    Modify equipment covered in training

--- permit/info/upgrades/woodworking_1.php
+++ permit/info/upgrades/woodworking_1.php
@@ -1,78 +1,88 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" >
-<head>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" /> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 
-<!-- ****************************************************************************
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+	<!-- ****************************************************************************
 **********															*************
 **********		INSERT PAGE TITLE BELOW within quotations!!!! 		*************
 **********		Title should be the same as the <h1> tag below 		*************
 **********															*************
 *********************************************************************************	-->
-				<?php $subtitle = "Woodworking 1 Upgrade";?>
+	<?php $subtitle = "Woodworking 1 Upgrade";?>
 
-            
-	<?php require ($_SERVER['DOCUMENT_ROOT']."/functions.php"); ?>
-    <?php require ($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
-    <?php require ($_SERVER['DOCUMENT_ROOT']."/main_nav.php"); ?>
 
-<!-- ************************* 				End of Navigation Items 			****************************  -->
+		<?php require ($_SERVER['DOCUMENT_ROOT']."/functions.php"); ?>
+			<?php require ($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+				<?php require ($_SERVER['DOCUMENT_ROOT']."/main_nav.php"); ?>
 
-<div id="wrapper">		<!-- wrapper sets the content area including the left nav, middle (main content), and right nav  -->
+					<!-- ************************* 				End of Navigation Items 			****************************  -->
 
-	<div id="leftNav">
+					<div id="wrapper">
+						<!-- wrapper sets the content area including the left nav, middle (main content), and right nav  -->
 
-		<?php include ("../../permit_left_nav.php") ?> <!-- Left Navigation in the include file -->
+						<div id="leftNav">
 
-		<h2 class="subnav2"><span>Related Links</span></h2>
-			<ul>
-				<li><a href="/permit/info/upgrades/welding_1.php">Welding 1 Upgrade</a></li>
-				<li><a href="/permit/info/upgrades/welding_2.php">Welding 2 Upgrade</a></li>				
-            		</ul>
-      	</div> <!-- END OF LEFT NAV DIV -->
-		
-	<div id="MainContent"> <!-- sets the main content (middle column area) -->
+							<?php include ("../../permit_left_nav.php") ?>
+								<!-- Left Navigation in the include file -->
 
-	  <h1>Woodworking 1 Upgrade</h1>
-                  
-      	<h2>Overview</h2>
-        	<p>The woodworking 1 upgrade is an additional amount of training individuals can take to learn how to safely use a variety of woodworking equipment (<a href="https://en.wikipedia.org/wiki/Table_saw" rel="external">table saw</a>, <a href="https://en.wikipedia.org/wiki/Panel_saw" rel="external">panel saw</a>, <a href="https://en.wikipedia.org/wiki/Circular_saw" rel="external">circular saw</a>, track saw, <a href="https://en.wikipedia.org/wiki/Miter_saw" rel="external">miter saw</a>, <a href="https://en.wikipedia.org/wiki/Biscuit_joiner" rel="external">biscuit joiner</a>, and <a href="https://en.wikipedia.org/wiki/Router_%28woodworking%29" rel="external">router</a>) in the Student Shop. Individuals will not be allowed to use this woodworking equipment until they have obtained the woodworking 1 upgrade.</p>
-            
-        <h2>Privileges</h2>
-        	<ul>
-            	<li>Use Shop table saws (both traditional and <a href="https://en.wikipedia.org/wiki/SawStop" rel="external">SawStop</a>)</li>
-                <li>Use the Shop panel saw</li>
-                <li>Use Shop circular saws</li>
-                <li>Use the Shop track saw</li>
-                <li>Use Shop miter saws</li>
-                <li>Use the Shop biscuit joiner</li>
-                <li>Use Shop wood routers</li>
-             </ul>
-        
-        <h2>Prerequisites</h2>
-        	<ul>
-            	<li>Red or green shop permit</li>
-            </ul>
-        
-        <h2>Requirements</h2>
-        	<p>(must be completed in order)</p>
-        	<ul>
-                <li><a href="https://emu.engr.wisc.edu/emu" rel="external">Apply for the wood shop upgrade</a>&#42;</li>
-                <li>Enroll in and attend the <a href="/permit/info/seminars/woodworking_1.php">wood shop seminar</a></li>
-            </ul>
-            <p>&#42;Individuals will only be able to see upgrades in EMU for which they have completed the prerequisites.</p>
-            
-         <h2>Time Frame</h2>
-         	<ul>
-				<li>Upgrades must be completed within 45 days of applying. Failure to meet the 45 day deadline will result in all progress towards the upgrade being removed</li>
-            </ul>            
-            <br />
-      
-	</div> <!-- END OF MAINCONTENT -->	
+								<h2 class="subnav2">
+									<span>Related Links</span>
+								</h2>
+								<ul>
+									<li><a href="/permit/info/upgrades/scotchman_1.php">Scotchman 1 Upgrade</a></li>
+									<li><a href="/permit/info/upgrades/woodworking_2.php">Woodworking 2 Upgrade</a></li>
+								</ul>
+						</div>
+						<!-- END OF LEFT NAV DIV -->
 
-</div> <!-- END OF WRAPPER -->
+						<div id="MainContent">
+							<!-- sets the main content (middle column area) -->
 
-<?php require ($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
+							<h1>Woodworking 1 Upgrade</h1>
 
-</body>
+							<h2>Overview</h2>
+							<p>The woodworking 1 upgrade is an additional amount of training individuals can take to learn the proper use of a variety of woodworking saws (<a href="https://en.wikipedia.org/wiki/Table_saw"
+								rel="external">table saw</a>, <a href="https://en.wikipedia.org/wiki/Panel_saw" rel="external">panel saw</a>, <a href="https://en.wikipedia.org/wiki/Circular_saw" rel="external">circular saw</a>, track saw, and <a href="https://en.wikipedia.org/wiki/Miter_saw"
+								rel="external">miter saw</a>) in the Student Shop. Individuals will not be allowed to use these saws until they have obtained the woodworking 1 upgrade.</p>
+
+							<h2>Privileges</h2>
+							<ul>
+								<li>Use Shop table saws (both traditional and <a href="https://en.wikipedia.org/wiki/SawStop" rel="external">SawStop</a>)</li>
+								<li>Use the Shop panel saw</li>
+								<li>Use Shop circular saws</li>
+								<li>Use the Shop track saw</li>
+								<li>Use Shop miter saws</li>
+							</ul>
+
+							<h2>Prerequisites</h2>
+							<ul>
+								<li>Red or green shop permit</li>
+							</ul>
+
+							<h2>Requirements</h2>
+							<p>(must be completed in order)</p>
+							<ul>
+								<li><a href="https://emu.engr.wisc.edu/emu" rel="external">Apply for the Woodworking 1 upgrade</a>&#42;</li>
+								<li>Enroll in and attend the <a href="/permit/info/seminars/woodworking_1.php">wood shop seminar</a></li>
+							</ul>
+							<p>&#42;Individuals will only be able to see upgrades in EMU for which they have completed the prerequisites.</p>
+
+							<h2>Time Frame</h2>
+							<ul>
+								<li>Upgrades must be completed within 45 days of applying. Failure to meet the 45 day deadline will result in all progress towards the upgrade being removed</li>
+							</ul>
+							<br />
+
+						</div>
+						<!-- END OF MAINCONTENT -->
+
+					</div>
+					<!-- END OF WRAPPER -->
+
+					<?php require ($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
+
+						</body>
+
 </html>
