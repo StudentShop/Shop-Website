96b8d3166ddf54a18597928fba17b90bfc6b1194
Charles Allhands <charles.allhands@wisc.edu>
2015-08-21 15:03:42 -0500

    Create charges and fees page

--- about/about_policies_left_nav.php
+++ about/about_policies_left_nav.php
@@ -1,11 +1,11 @@
 
-        
+
        <h2 class="subnav"><span><a href="/about/policies.php">Policies</a></span></h2> <!-- needed for accessibility standards purposes; display is set to none - invisible -->
               <ul>
               	  <li><a href="/about/safetyanduse.php">Safety &amp Use</a></li>
                   <li><a href="/about/machinereservation.php">Machine Reservation</a></li>
                   <li><a href="/about/toolcheckinout.php">Tool Check in/out</a></li>
+                  <li><a href="/about/fees.php">Charges/Fees</a></li>
                   <li><a href="/about/warning.php">Warning Policy</a></li>
                   <li><a href="/about/bannedmaterials.php">Banned Materials</a></li>
               </ul>
-	
---
+++ about/fees.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+  <!-- ****************************************************************************
+         **********                                                     *************
+         **********    INSERT PAGE TITLE BELOW within quotations!!!!    *************
+         **********   Title should be the same as the <h1> tag below    *************
+         **********                                                     *************
+         **************************************************************************** -->
+  <?php $subtitle = "Charges/Fees";?>
+
+    <?php require ($_SERVER['DOCUMENT_ROOT']."/functions.php"); ?>
+      <?php require ($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+        <?php require ($_SERVER['DOCUMENT_ROOT']."/main_nav.php"); ?>
+
+          <!-- ************************* End of Navigation Items ****************************  -->
+
+          <div id="wrapper">
+            <!-- wrapper sets the content area including the left nav, middle (main content), and right nav  -->
+
+            <div id="leftNav">
+              <?php include ("about_policies_left_nav.php") ?>
+                <!-- Left Navigation in the include file -->
+
+                <!--<h2> tag is needed for formatting and accessibility purposes -->
+                <!--<h2 class="subnav2"><span>Related Links</span></h2>
+            <ul>
+              <li><a href="#">----</a></li>
+              <li><a href="#">----</a></li>
+              <li><a href="#">----</a></li>
+            </ul> -->
+            </div>
+            <!-- End of leftNav -->
+
+            <div id="MainContent">
+              <!-- sets the main content (middle column area) -->
+
+              <h1>Charges &amp; Fees</h1>
+
+              <h2>Shop Access Fee</h2>
+
+              <p>Starting fall semester 2015, the College is implementing a Shop material fee of $50 (per user, per semester) to cover the costs of consumable supplies used by students when accessing the Shop for projects.</p>
+
+              <p>The College of Engineering Student Shop utilizes the engineering undergraduate differential tuition to help pay for new equipment as well as equipment maintenance and repair. Differential tuition has made it possible to increase the number of equipment pieces in the shop, keep equipment current and allow for increased access to Shop equipment for students.</p>
+
+              <p>This fee covers the cost of standard CoE Student Shop stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables such as copper, stainless steel, sheet metal, etc. will be charged a separate fee on a per use basis.</p>
+
+              <h2>Machine Reservation Charge</h2>
+              <p>Your text here.</p>
+
+            </div>
+            <!-- END OF MAINCONTENT -->
+
+          </div>
+          <!-- END OF WRAPPER -->
+
+          <?php require ($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
+
+            </body>
+
+</html>
