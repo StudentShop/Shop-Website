b2c7ad918ce43c16eed16c969e8fccd5182e6121
Charles Allhands <charles.allhands@wisc.edu>
2015-10-09 10:24:38 -0500

    Remove job opening

--- about/jobs.php
+++ about/jobs.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" >
 <head>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" /> 
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 
 <!-- ****************************************************************************
 **********															*************
@@ -11,7 +11,7 @@
 *********************************************************************************	-->
 				<?php $subtitle = "Jobs";?>
 
-            
+
     <?php require ($_SERVER['DOCUMENT_ROOT']."/functions.php"); ?>
     <?php require ($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
     <?php require ($_SERVER['DOCUMENT_ROOT']."/main_nav.php"); ?>
@@ -22,61 +22,61 @@
 
 
 	<div id="leftNav">
-    
+
 		<?php include ("about_left_nav.php") ?> <!-- Left Navigation in the include file -->
             <!-- begin right navigation items	<h2> tag is needed for formatting and accessibility purposes -->
               <h2 class="subnav2"><span>Related Links</span></h2>
                   <ul>
-                      <li><a href="http://jobcenter.wisc.edu/">UW Job Center</a></li>                      
+                      <li><a href="http://jobcenter.wisc.edu/">UW Job Center</a></li>
                   </ul>
-                  
+
 	</div> <!-- END OF LEFTNAV -->
 
 
 	<div id="MainContent"> <!-- sets the main content (middle column area) -->
-    	
+
         <img src="images/job_openings.png" alt="illustrative picture of jobs" align="right" />
     	<h1>Jobs</h1>
         	<p>When we have job opportunities available we will post them here as well as on the <a href="http://jobcenter.wisc.edu/" rel="external">UW Job Center's website</a>.</p>
             <h2>Currently Available Positions:</h2>
-            	
+
             <div id="jobs">
-                <!--<p>None</p>-->
-                <h3>Student Shop Technician</h3>
+                <p>None</p>
+                <!--<h3>Student Shop Technician</h3>
                     	<p>Individuals that enjoy working in a hands-on work environment and have experience
 in the use of machine shop equipment (including lathes, mills, hand tools and power tools).
 Applicants must also be capable of performing physical labor and have
 general computer skills, Microsoft Office, CAD/CAM experience is helpful. Would like to find people
 that are looking for a position that would be available to them for their years here at
 the UW. We are looking for people to work fall and the spring
-semesters (mornings preferred). Full time summer employment may also be available. Applicant must be a UW-Madison full-time 
-student enrolled in the current semester.</p>                
-           			<h4>Responsibilities include:</h4> 
+semesters (mornings preferred). Full time summer employment may also be available. Applicant must be a UW-Madison full-time
+student enrolled in the current semester.</p>
+           			<h4>Responsibilities include:</h4>
             			<ul>
-                        	<li>Working in the CoE Student Shop and Tool Crib.</li> 
+                        	<li>Working in the CoE Student Shop and Tool Crib.</li>
 							<li>Checking out tools and equipment</li>
                             <li>Working with students in the shop on various projects</li>
 							<li>Maintenance of shop machines and equipment, moving of equipment and
 material along with other miscellaneous tasks.</li>
 						</ul>
             		<h4>Available hours per week:</h4>
-            			<p>10 – 20 hours, between 8am and 11pm Monday - Friday and some Saturdays</p>
+            			<p>10 - 20 hours, between 8am and 11pm Monday - Friday and some Saturdays</p>
 					<h4>Hourly Wage:</h4>
-                    	<p>$9.50 - $10.50</p>                                                
+                    	<p>$9.50 - $10.50</p>
 					<h4>How to apply:</h4>
 						<p>Send a cover letter and resume to Larry Wheeler (<a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#119;&#104;&#101;&#101;&#108;&#101;&#114;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">&#119;&#104;&#101;&#101;&#108;&#101;&#114;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;</a>), fax it to (608) 261-1111, or drop off the
-information at the CoE Student Shop <a href="/about/floorplan.php">Rm B1086</a> <a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=1550+Engineering+Dr+Madison,+WI+53706&sll=43.072304,-89.412133&sspn=0.011176,0.027874&ie=UTF8&hq=&hnear=1550+Engineering+Dr,+Madison,+Dane,+Wisconsin+53706&view=map">1550 Engineering Dr Madison, WI 53706</a></p>
+information at the CoE Student Shop <a href="/about/floorplan.php">Rm B1086</a> <a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=1550+Engineering+Dr+Madison,+WI+53706&sll=43.072304,-89.412133&sspn=0.011176,0.027874&ie=UTF8&hq=&hnear=1550+Engineering+Dr,+Madison,+Dane,+Wisconsin+53706&view=map">1550 Engineering Dr Madison, WI 53706</a></p> -->
 
 				<!--<h3>Webmaster</h3>
-                    	<p>The CoE Student Shop is in search of a new Webmaster. Applicant must be a UW-Madison full-time student enrolled in the 2010 Fall semester. We are looking for a person to work this semester along with next school year (11-12) or hopefully longer. Proficiencies in software such as Dreamweaver CS4,  and Photoshop CS4 are essential. Applicant should be able to read and understand HTML (including some HTML5), XHTML, CSS, and be able to manipulate simple JQuery and PHP. Applicant should be highly creative, have a keen eye-for-design, be able to work in a group, and have a strong desire to work within commonly accepted web standards (as determined by the W3C and the University of Wisconsin). Willingness to learn on the job is must.</p>                
-           			<h4>Responsibilities include:</h4> 
+                    	<p>The CoE Student Shop is in search of a new Webmaster. Applicant must be a UW-Madison full-time student enrolled in the 2010 Fall semester. We are looking for a person to work this semester along with next school year (11-12) or hopefully longer. Proficiencies in software such as Dreamweaver CS4,  and Photoshop CS4 are essential. Applicant should be able to read and understand HTML (including some HTML5), XHTML, CSS, and be able to manipulate simple JQuery and PHP. Applicant should be highly creative, have a keen eye-for-design, be able to work in a group, and have a strong desire to work within commonly accepted web standards (as determined by the W3C and the University of Wisconsin). Willingness to learn on the job is must.</p>
+           			<h4>Responsibilities include:</h4>
             			<ul>
-                        	<li>Updating and improving current web site</li> 
+                        	<li>Updating and improving current web site</li>
 							<li>Designing new web pages using CSS (NO TABLE LAYOUTS)</li>
                             <li>Creating various training materials (which may require the use of Photoshop CS4, Captivate, PowerPoint, Word, Acrobat)</li>
 						</ul>
             		<h4>Available hours per week:</h4>
-            			<p>10 – 20 hours, between 8am and 11pm Monday through Friday (Summer Also Available 8am - 4pm Monday through Friday)</p>                        
+            			<p>10 ï¿½ 20 hours, between 8am and 11pm Monday through Friday (Summer Also Available 8am - 4pm Monday through Friday)</p>
 					<h4>Hourly Wage:</h4>
                     	<p>$11 - $15 </p>
                     <h4>How to apply:</h4>
@@ -86,10 +86,10 @@ information at the CoE Student Shop <a href="/about/floorplant.php">Rm B1086</a>
 
 
     </div> <!-- jobs -->
-    
-    
+
+
 	</div> <!-- END OF MAINCONTENT -->
-	
+
 </div> <!-- END OF WRAPPER -->
 
     <?php require ($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
