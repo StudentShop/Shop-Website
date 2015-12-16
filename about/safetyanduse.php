fe74e79e98f05929eebc439b28b3bdcfb2be29b2
Charles Allhands <charles.allhands@wisc.edu>
2015-12-16 09:38:16 -0600

    Update shop safety and use policies

--- about/safetyanduse.php
+++ about/safetyanduse.php
@@ -1,98 +1,119 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" >
-<head>
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 
-<!-- ****************************************************************************
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+  <!-- ****************************************************************************
 **********                                                                                                                      *************
 **********              INSERT PAGE TITLE BELOW within quotations!!!!           *************
 **********              Title should be the same as the <h1> tag below          *************
 **********                                                                                                                      *************
 *********************************************************************************       -->
-                                <?php $subtitle = "Safety and Use";?>
+  <?php $subtitle = "Safety and Use";?>
 
 
-                <?php include ("../functions.php") ?>
-                <?php include("../header.php"); ?>
-                <?php include("../main_nav.php"); ?>
+    <?php include ("../functions.php") ?>
+      <?php include("../header.php"); ?>
+        <?php include("../main_nav.php"); ?>
 
-<!-- *************************                          End of Navigation Items                         ****************************  -->
+          <!-- *************************                          End of Navigation Items                         ****************************  -->
 
-<div id="wrapper">              <!-- wrapper sets the content area including the left nav, middle (main content), and right nav  -->
+          <div id="wrapper">
+            <!-- wrapper sets the content area including the left nav, middle (main content), and right nav  -->
 
-        <div id="leftNav">
-                <?php include ("about_policies_left_nav.php") ?> <!-- Left Navigation in the include file -->
+            <div id="leftNav">
+              <?php include ("about_policies_left_nav.php") ?>
+                <!-- Left Navigation in the include file -->
                 <!-- begin right navigation items       <h2> tag is needed for formatting and accessibility purposes -->
-              <!--<h2 class="subnav2"><span>Related Links</span></h2>
+                <!--<h2 class="subnav2"><span>Related Links</span></h2>
                   <ul>
                       <li><a href="#">----</a></li>
                       <li><a href="#">----</a></li>
                       <li><a href="#">----</a></li>
                   </ul> -->
-        </div> <!-- End leftNav -->
+            </div>
+            <!-- End leftNav -->
 
-        <div id="MainContent"> <!-- sets the main content (middle column area) -->
+            <div id="MainContent">
+              <!-- sets the main content (middle column area) -->
 
-        <h1>Safety and Use</h1>
-                        <p>To ensure safe practices in the shop and shop controlled areas the College of Engineering has adopted the following rules. These rules will be consistently enforced and non-compliance will result in the suspension of shop privileges or disciplinary action. It is the belief of the College of Engineering that having an understanding of the inherent hazards of working in a shop area should be an integral and important part of the educational process.</p>
+              <h1>Safety and Use</h1>
+              <p>To ensure safe practices in the shop and shop controlled areas the College of Engineering has adopted the following rules. These rules will be consistently enforced and non-compliance will result in the suspension of shop privileges or disciplinary
+                action. It is the belief of the College of Engineering that having an understanding of the inherent hazards of working in a shop area should be an integral and important part of the educational process. The following does not constitute
+                an exhaustive list of rules. What is appropriate in the shop is always at the discretion of shop staff.</p>
 
-        <h2>General Shop Rules</h2>
-                        <ol>
+              <h2>General Shop Rules</h2>
+              <ol>
                 <li>The College of Engineering Student Shop is for use only by students currently registered for a UW CoE course, CoE staff and CoE faculty.</li>
-                                <li>No one under 18 years of age may be in the Student Shop areas.</li>
-                                <li>No one may be in the Student Shop without a permit for any reason. Any individual working in the shop, attending a seminar, or checking out tools must have their permit visible.</li>
+                <li>No one under 18 years of age may be in the Student Shop areas.</li>
+                <li>No one may be in the Student Shop without a permit for any reason. Any individual working in the shop, attending a seminar, or checking out tools must have their permit visible.</li>
                 <li>Using a permit under false pretenses (e.g. using a forged permit or representing someone else's permit as your own) or aiding or abetting someone using a permit under false pretenses will result in permanent suspension from the shop.</li>
-                                <li>The Student Shop is for projects related to research or class work. Personal projects are not allowed.</li>
-                                <li>Shop facilities may not be used unless the Student Shop Supervisor or his/her delegated representative is present.</li>
+                <li>The Student Shop is for projects related to research or class work. Personal projects are not allowed.</li>
+                <li>Shop facilities may not be used unless the Student Shop Supervisor or his/her delegated representative is present.</li>
                 <li>There must be at least two individuals present at all times when the shop facilities are in use.</li>
-                                <li>Smoking, chewing tobacco, or being under the influence of drugs or alcohol is prohibited.</li>
-                                <li>Personal hygiene must reach the threshold of social acceptability.</li>
-                                <li>Eating or drinking around equipment is prohibited.</li>
-                                <li>All accidents, including minor injuries and all hazardous conditions or activities must be reported immediately to the Student Shop Supervisor.</li>
-            </ol>
+                <li>Smoking, chewing tobacco, or being under the influence of drugs or alcohol is prohibited.</li>
+                <li>Personal hygiene must reach the threshold of social acceptability.</li>
+                <li>Eating or drinking around equipment is prohibited.</li>
+                <li>All accidents, including minor injuries and all hazardous conditions or activities must be reported immediately to the Student Shop Supervisor.</li>
+              </ol>
 
-                <h2><a name="appClothing"></a>Appropriate Clothing</h2>
-                <ol>
-                                <li>Any clothing, jewelry (e.g. rings, watches, dangling earrings, bracelets, necklaces, etc.), earphone cords (i.e. iPods, etc.) or other items that could become entangled in moving machinery are prohibited.</li>
-                <li>Shop users must at minimum wear clothes such that there is no skin visible below the base of the neck with the exception of arms/hands below the elbow (unless when welding or using an angle grinder or similar tool with equivalent safety concerns).</li>
+              <h2>
+                <a name="appClothing"></a>Appropriate Clothing</h2>
+              <ol>
+                <li>Any clothing, jewelry (e.g. rings, watches, dangling earrings, bracelets, necklaces, etc.), earphone cords (i.e. iPods, etc.) or other items that could become entangled in moving machinery are prohibited.</li>
+                <li>Shop users must at minimum wear clothes such that there is no skin visible below the base of the neck with the exception of arms/hands below the elbow (unless when welding or using an angle grinder or similar tool with equivalent safety
+                  concerns). Clothing that resists light cuts and does not melt when burned (e.g. cotton jeans, canvas pants) is preferred but not required (unless working with a heat source, such as when welding). Leggings and similar synthetic fabrics
+                  are discouraged.
+                </li>
+                <li>Shop users must at minimum wear shoes that completely enclose their feet and do not increase the likelihood of a fall (e.g. high heel shoes would not be acceptable). Non-slip, steel toed boots are preferred but not required.</li>
                 <li>Long hair (i.e. exceeding shoulder length) must be adequately restrained to prevent becoming entangled in moving machinery. Facial hair which extends beyond the second button from top of a collared shirt must also be similarly restrained.</li>
-                                <li>Loose veils/hijabs are not allowed in the Student Shop. Any veil/hijab that a student wants to wear in the shop must be approved by Student Shop staff.</li>
-                                <li>Always remove gloves before turning on or operating any machine. Gloves are generally discouraged when working in the shop. If material is rough or sharp and gloves must be worn, place or handle material with the machine turned off (all moving parts stopped).</li>
-             </ol>
+                <li>Loose veils/hijabs are not allowed in the Student Shop. Any veil/hijab that a student wants to wear in the shop must be approved by Student Shop staff.</li>
+                <li>Always remove gloves before turning on or operating any machine. Gloves are generally discouraged when working in the shop. If material is rough or sharp and gloves must be worn, place or handle material with the machine turned off (all
+                  moving parts stopped).</li>
+              </ol>
 
-                <h2>General Safety Rules</h2>
-                <ol>
-                                <li>Eye protection must be properly worn at all times by all individuals in the Student Shop areas. Always wear safety glasses or face shields (required when using hand grinders) designed for the type of work being done. Safety glasses must still be worn when using face shields or welding helmets. The face shields used when grinding have built in hearing protection as well.</li>
-                                <li>Hearing protection is required when working with machines or tools that produce an excessive (90db or greater) noise level for extended periods of time. Hearing (ear) protection may be checked out from the Student Shop.</li>
-                                <li>Causing distractions of any kind is forbidden (e.g. yelling, screaming, making sudden loud noises, etc.). Students are expected to demonstrate mature judgment and common sense in their work and conduct while working in the shop area.</li>
-                                <li>Concentrate on the work and do not talk unnecessarily while operating the shop machines.</li>
-                                <li>Cell Phones and other electronic devices should be turned off and not used in the shop.</li>
-                                <li>Tools are to be used only for the purpose for which they are designed.</li>
-                                <li>You may not operate equipment that you have not received permission to use.</li>
-                                <li>If you are not sure how to operate something check with the Shop Staff.</li>
-                                <li>Read and follow all operational signs and posting located on or near the various machines. This should be done before ever turning on the machine.</li>
-                                <li>Machine guards are to be in place and in use at all times when equipment is operating.</li>
-                <li>Do not try to stop a machine�s rotating parts with your hands, especially a lathe or drill chuck.</li>
+              <h2>General Safety Rules</h2>
+              <ol>
+                <li>Eye protection must be properly worn at all times by all individuals in the Student Shop areas. Always wear safety glasses or face shields (required when using hand grinders) designed for the type of work being done. Safety glasses must
+                  still be worn when using face shields or welding helmets.</li>
+                <li>Hearing protection is required when working with machines or tools that produce an excessive (90db or greater) noise level for extended periods of time (e.g. when using hand grinders). Hearing (ear) protection may be checked out from the
+                  Student Shop.
+                </li>
+                <li>Causing distractions of any kind is forbidden (e.g. yelling, screaming, making sudden loud noises, etc.). Students are expected to demonstrate mature judgment and common sense in their work and conduct while working in the shop area.</li>
+                <li>Concentrate on the work and do not talk unnecessarily while operating the shop machines.</li>
+                <li>Cell Phones and other electronic devices should be turned off and not used in the shop.</li>
+                <li>Tools are to be used only for the purpose for which they are designed.</li>
+                <li>You may not operate equipment that you have not received permission to use.</li>
+                <li>If you are not sure how to operate something check with shop staff.</li>
+                <li>Read and follow all operational signs and posting located on or near the various machines. This should be done before ever turning on the machine.</li>
+                <li>Machine guards are to be in place and in use at all times when equipment is operating.</li>
+                <li>Do not try to stop a machine's rotating parts with your hands, especially a lathe or drill chuck.</li>
                 <li>Do not leave a machine until it has come to a complete stop.</li>
                 <li>Make sure the machine is off and unplugged before making any adjustments or repairs (e.g changing blades on the table saw).</li>
-                                <li>Always stop the machine to make speed or feed changes that require the shifting of a gear lever. <strong>Note</strong>: Variable speed machines � mills, drill presses and lathes must be running when adjusting the speed. The machine still must be off though to switch from low to high gear or vice versa.</li>
-                <li>Use a blue chip brush (located by machines) to remove short, discontinuous types of chips � not hands, fingers or rags. Use a pair of pliers to remove chips, especially the long, stringy type.</li>
-                                <li>Never use rags to clean the work piece or any part of the machine that is running.</li>
-                                <li>Only one person may operate a machine at a time.</li>
-                                <li>Keep aisles clear and maintain unobstructed access to all exits, first aid kits, fire extinguishers, electrical panels, emergency showers and eyewash stations. Take note of their location in the shops areas.</li>
+                <li>Always stop the machine to make speed or feed changes that require the shifting of a gear lever.
+                  <strong>Note</strong>: Variable speed machines such as mills, drill presses and lathes must be running when adjusting the speed. The machine still must be off though to switch from low to high gear or vice versa.</li>
+                <li>Use a blue chip brush (located by machines) to remove short, discontinuous types of chips, not hands, fingers or rags. Use a pair of pliers to remove chips, especially the long, stringy type.</li>
+                <li>Never use rags to clean the work piece or any part of the machine that is running.</li>
+                <li>Only one person may operate a machine at a time.</li>
+                <li>Keep aisles clear and maintain unobstructed access to all exits, first aid kits, fire extinguishers, electrical panels, emergency showers and eyewash stations. Take note of their location in the shops areas.</li>
                 <li>Keep floors free of loose tools, oil, grease or any other type of liquid. Clean up spills immediately; they are slipping hazards.</li>
                 <li>Keep the floor clear of metal chips and scrap pieces. Put them in the trash containers or the scrap metal bins.</li>
                 <li>Compressed air is not to be used to clean off machines, work areas, tools or clothing. If air is used to clean a part, point the air hose down and away from yourself or any other person.</li>
-                <li>All spray painting in the Shops controlled areas must be done in the approved spray booth (B1062). See Shop Staff for access.</li>
+                <li>All spray painting in the Shop's controlled areas must be done in the approved spray booth (B1062). See shop staff for access.</li>
                 <li>Defective, dull or broken tools may cause injury if used. Turn them in to the Student Shop staff for replacement or sharpening.</li>
-            </ol>
+                <li>Notify shop staff immediately if blood is found. Do not attempt to clean up blood or other bodily fluids unless you have recieved University blood-borne pathogen training.</li>
+              </ol>
 
 
-        </div> <!-- END OF MAINCONTENT -->
+            </div>
+            <!-- END OF MAINCONTENT -->
 
-</div> <!-- END OF WRAPPER -->
+          </div>
+          <!-- END OF WRAPPER -->
 
-<?php include("../footer.php"); ?>
+          <?php include("../footer.php"); ?>
 
-</body>
+            </body>
+
 </html>
