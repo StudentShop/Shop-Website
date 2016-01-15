f0f1d723267ad3a7269cdcc6f7bf1123cfe22e0b
Charles Allhands <charles.allhands@wisc.edu>
2016-01-15 16:14:39 -0600

    Improve wording

--- about/fees.php
+++ about/fees.php
@@ -43,39 +43,39 @@
               <h2><a name="materialsfee"></a>Shop Materials Fee</h2>
 
               <p>All shop users (note exceptions listed below) are required to pay a biannual materials fee of $50 to use the Student Shop (i.e. $100 per year, per user). The first period extends from December through June and the second extends from June through December (allowing a one month overlap at the start and end of each fee period).</p>
-              <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables, such as copper, stainless steel, sheet metal, etc. must be purchased a la carte.</p>
+              <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables, such as copper, stainless steel, sheet metal, etc. must be purchased as needed.</p>
 
               <h3>Exceptions</h3>
               <ul>
-                <li>All faculty and university staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies) are exempt from the materials fee.</li>
+                <li>All faculty and University staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies) are exempt from the materials fee.</li>
                <li>Teaching assistants for courses that use the shop may be entitled to have the fee waived. At this time that requires the professor of the course for which they act as a teaching assistant to <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a> with the teaching assistant's name and a means of identification (e.g., a campus ID number or NetID).</li>
               <li>Those working on funded research are required to pay the research fee (see below) rather than the materials fee.</li>
               <li>Individuals with a permit may check out hand tools (not including tooling, machine tools, or electric or pneumatic power tools) without paying the materials fee.</li>
-              <li>Enlisting Shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require the fee (nor even a permit).</li></ul>
+              <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the fee (nor even a permit).</li></ul>
 
               <h2><a name="researchfee"></a>Research Fee</h2>
               <p>Individuals working on funded research are required to pay a biannual research fee of $100 to use the Student Shop (i.e. $200 per year, per user). The first period extends from December through June and the second extends from June through December (allowing a one month overlap at the start and end of each fee period).</p>
-              <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables such as copper, stainless steel, sheet metal, etc. must be purchased a la carte. This fee also covers repair, maintenance, and upgrading of shop equipment.</p>
+              <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables such as copper, stainless steel, sheet metal, etc. must be purchased as needed. This fee also covers repair, maintenance, and upgrading of shop equipment.</p>
 
               <h3>Exceptions</h3>
               <ul>
-                <li>All faculty and university staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies) are exempt from the research fee.</li>
+                <li>All faculty and University staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies) are exempt from the research fee.</li>
               <li>Individuals with a permit may check out hand tools (not including tooling, machine tools, or electric or pneumatic power tools) without paying the materials fee.</li>
-              <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require the fee (nor even a permit).</li></ul>
+              <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the fee (nor even a permit).</li></ul>
 
               <h2><a name="feefaq"></a>Frequently Asked Questions</h2>
               <h3>How do I pay the fee?</h3>
               <p>To pay the materials fee, <a href="https://emu.engr.wisc.edu/emu">log into EMU</a> and click the button at the top of the screen that says "Pay Fee Now". You will need a credit card to pay.</p>
-            <p>To pay the research fee, have your professor or principal investigator (PI) <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include your name, a means of identification (e.g., your campus ID number or your NetID), and a university funding account to charge. There is no online payment method available at this time.</p>
+            <p>To pay the research fee, have your professor or principal investigator (PI) <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include your name, a means of identification (e.g., your campus ID number or your NetID), and a University funding account to charge. There is no online payment method available at this time.</p>
 
             <h3>I tried to pay online with a credit card but the system rejected my card. How do I pay?</h3>
-            <p>Unfortunately, online payment via credit card is the only way to pay the materials fee. Most likely you entered incorrect information or used auto fill. If you don't think that's the case your only option is to use a different credit card (maybe you could use a friend's or parent's and reimburse them?)
+            <p>Unfortunately, online payment via credit card is the only way to pay the materials fee. Most likely you entered incorrect information or used auto fill. If you don't think that's the case your only option is to use a different credit card (maybe you could use a friend's or parent's card and reimburse them?).</p>
 
               <h3>I'm a student working on a funded research project in the shop. Which fee do I pay?</h3>
               <p>Funded research must pay the research fee instead of the materials fee.</p>
 
               <h3>Can I pay with cash, Wiscard account, check, bitcoin, etc.?</h3>
-              <p>Unfortunately the only accepted method of payment for the materials fee is a credit card. We are exploring other options but they are not available at this time. The research fee can only be paid with a university funding string.</p>
+              <p>Unfortunately the only accepted method of payment for the materials fee is a credit card. We are exploring other options but they are not available at this time. The research fee can only be paid with a University funding account.</p>
 
               <h3>Can I pay with a ProCard?</h3>
               <p>Please consult with your departmental financial person to verify that this is an acceptable use of the <a href="http://www.bussvc.wisc.edu/acct/purchcd/">purchasing card</a>.</p>
@@ -87,10 +87,10 @@
               <p>Refunds will only be given in extenuating circumstances. Refund decisions are made by the <a href="http://www.engr.wisc.edu/leadership.html">office of the Dean</a>.</p>
 
               <h3>Do I have to pay a fee before submitting a work order for the Instrument Shop, a 3D printing request, etc.?</h3>
-              <p>No. Services provided by the shop where you have to pay for the work being done do not require payment of the materials or research fee. If the work is being done for free either the material or research fee applies.</p>
+              <p>No, services provided by the shop where you have to pay for the work being done do not require payment of the materials or research fee. If the work is being done free of charge either the material or research fee applies.</p>
 
               <h3>Can I pay on behalf of a student working for me?</h3>
-              <p>Yes. If you are paying the research fee <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include the student's name, a means of identification (e.g., a campus ID number or NetID), and a funding account to charge.</p>
+              <p>Yes, if you are paying the research fee <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include the student's name, a means of identification (e.g., their campus ID number or NetID), and a University funding account to charge.</p>
               <p>You can request the fee be waived for teaching assistants if they are working for a course that uses the shop. Email Gene Masters and include the name of the teaching assistant, a means of identification (such as their Campus ID or NetID), and the course they are working for.</p>
               <p>If you are paying a student's materials fee you will need them to log into <a href="https://emu.engr.wisc.edu/emu">EMU</a> and click the "Pay Fee Now" button and then enter the credit card information for them.</p>
 
@@ -98,7 +98,7 @@
                <p>No, you are academic staff.</p>
 
               <h3>I'm a visiting scholar. Do I need to pay a fee?</h3>
-              <p>No. If they system says you do, please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a></p>
+              <p>No, if the system says you do, please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
 
               <h3>I paid the materials fee but now I work on a research project. Do I have to pay the research fee as well?</h3>
               <p>Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
@@ -107,7 +107,7 @@
               <p>Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
 
               <h3>How can I get my fee waived?</h3>
-              <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. They must have the professor of the course email Gene Masters to request the fee be waived. The professor must include the name of the teaching assistant, a means of identifying them (such as their Campus ID or NetID), and the course they are working for.</p>
+              <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. They must have the professor of the course <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a> to request the fee be waived. The professor must include the name of the teaching assistant, a means of identifying them (e.g., Campus ID or NetID), and the course they are working for.</p>
 
               <h3>I just want training. Shouldn't I be exempt from the fee since I'm not using any materials?</h3>
               <p>No, nearly all training in the shop makes use of materials.</p>
@@ -131,7 +131,7 @@
               <p>Unfortunately, no. The month overlap at the start and end of each period is supposed to reduce the chances of this happening. However, if you are outside of the one month overlap but need shop access for both the current period and the next one, you will have to pay for both.</p>
 
               <h3>Since this fee covers materials used in the shop, I don't have to pay for broken or lost equipment then?</h3>
-              <p>No. If you damage expensive shop equipment you will still be liable for it's replacement. Some broken tooling may be covered by the fee. This is at the discretion of shop staff.</p>
+              <p>No, if you damage expensive shop equipment you will still be liable for it's replacement. Some broken tooling may be covered by the fee. This is at the discretion of shop staff.</p>
 
               <h3>My question wasn't answered in the FAQ. Whom do I contact?</h3>
               <p>Our apologies, please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a> with your question.</p>
