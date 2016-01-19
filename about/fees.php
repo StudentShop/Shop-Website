6aa64377dcc65d11beb44afc8ed399da8f6b6e9e
Charles Allhands <charles.allhands@wisc.edu>
2016-01-19 10:55:43 -0600

    Improving wording and order

--- about/fees.php
+++ about/fees.php
@@ -65,75 +65,75 @@
 
               <h2><a name="feefaq"></a>Frequently Asked Questions</h2>
               <h3>How do I pay the fee?</h3>
-              <p>To pay the materials fee, <a href="https://emu.engr.wisc.edu/emu">log into EMU</a> and click the button at the top of the screen that says "Pay Fee Now". You will need a credit card to pay.</p>
+              <p>To pay the materials fee, <a href="https://emu.engr.wisc.edu/emu">log into EMU</a> and click the button at the top of the screen that says "Pay Fee Now". You will need a credit card to pay. A debit card set up to work as a credit card will also work.</p>
             <p>To pay the research fee, have your professor or principal investigator (PI) <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include your name, a means of identification (e.g., your campus ID number or your NetID), and a University funding account to charge. There is no online payment method available at this time.</p>
 
             <h3>I tried to pay online with a credit card but the system rejected my card. How do I pay?</h3>
             <p>Unfortunately, online payment via credit card is the only way to pay the materials fee. Most likely you entered incorrect information or used auto fill. If you don't think that's the case your only option is to use a different credit card (maybe you could use a friend's or parent's card and reimburse them?).</p>
 
-              <h3>I'm a student working on a funded research project in the shop. Which fee do I pay?</h3>
+            <h3>I'm a student working on a funded research project in the shop. Which fee do I pay?</h3>
               <p>Funded research must pay the research fee instead of the materials fee.</p>
 
-              <h3>Can I pay with cash, Wiscard account, check, bitcoin, etc.?</h3>
+            <h3>Can I pay with cash, Wiscard account, check, bitcoin, etc.?</h3>
               <p>Unfortunately the only accepted method of payment for the materials fee is a credit card. We are exploring other options but they are not available at this time. The research fee can only be paid with a University funding account.</p>
 
-              <h3>Can I pay with a ProCard?</h3>
+            <h3>Can I pay with a ProCard?</h3>
               <p>Please consult with your departmental financial person to verify that this is an acceptable use of the <a href="http://www.bussvc.wisc.edu/acct/purchcd/">purchasing card</a>.</p>
 
-              <h3>Can I be reimbursed by my department?</h3>
+              <h3>How can I get my fee waived?</h3>
+                <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. They must have the professor of the course <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a> to request the fee be waived. The professor must include the name of the teaching assistant, a means of identifying them (e.g., Campus ID or NetID), and the course they are working for.</p>
+                
+            <h3>Can I be reimbursed by my department?</h3>
               <p>That is a departmental decision; however, you should not pay the fee yourself and then ask to be reimbursed. Speak with your department's financial person or your professor first and find out how they would like to pay.</p>
 
-              <h3>Can I have a refund?</h3>
+            <h3>Can I have a refund?</h3>
               <p>Refunds will only be given in extenuating circumstances. Refund decisions are made by the <a href="http://www.engr.wisc.edu/leadership.html">office of the Dean</a>.</p>
 
-              <h3>Do I have to pay a fee before submitting a work order for the Instrument Shop, a 3D printing request, etc.?</h3>
+            <h3>Do I have to pay a fee before submitting a work order for the Instrument Shop, a 3D printing request, etc.?</h3>
               <p>No, services provided by the shop where you have to pay for the work being done do not require payment of the materials or research fee. If the work is being done free of charge either the material or research fee applies.</p>
 
-              <h3>Can I pay on behalf of a student working for me?</h3>
+            <h3>Can I pay on behalf of a student working for me?</h3>
               <p>Yes, if you are paying the research fee <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include the student's name, a means of identification (e.g., their campus ID number or NetID), and a University funding account to charge.</p>
               <p>You can request the fee be waived for teaching assistants if they are working for a course that uses the shop. Email Gene Masters and include the name of the teaching assistant, a means of identification (such as their Campus ID or NetID), and the course they are working for.</p>
               <p>If you are paying a student's materials fee you will need them to log into <a href="https://emu.engr.wisc.edu/emu">EMU</a> and click the "Pay Fee Now" button and then enter the credit card information for them.</p>
 
-              <h3>I'm emeritus faculty. Do I need to pay a fee?</h3>
+            <h3>I'm emeritus faculty. Do I need to pay a fee?</h3>
                <p>No, you are academic staff.</p>
 
-              <h3>I'm a visiting scholar. Do I need to pay a fee?</h3>
+            <h3>I'm a visiting scholar. Do I need to pay a fee?</h3>
               <p>No, if the system says you do, please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
 
-              <h3>I paid the materials fee but now I work on a research project. Do I have to pay the research fee as well?</h3>
+            <h3>I paid the materials fee but now I work on a research project. Do I have to pay the research fee as well?</h3>
+              <p>Individuals working in the shop on funded research must pay the research fee. In a case like this the research group should pay the difference of fees to the shop. Ideally, they should also reimburse the individual who paid the materials fee but that decision is up to the research group.</p>
+
+            <h3>One of my students paid the fee before I was able to have the fee waived. What can I do?</h3>
               <p>Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
 
-              <h3>One of my students paid the fee before I was able to have the fee waived. What can I do?</h3>
-              <p>Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
-
-              <h3>How can I get my fee waived?</h3>
-              <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. They must have the professor of the course <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a> to request the fee be waived. The professor must include the name of the teaching assistant, a means of identifying them (e.g., Campus ID or NetID), and the course they are working for.</p>
-
-              <h3>I just want training. Shouldn't I be exempt from the fee since I'm not using any materials?</h3>
+            <h3>I just want training. Shouldn't I be exempt from the fee since I'm not using any materials?</h3>
               <p>No, nearly all training in the shop makes use of materials.</p>
 
-              <h3>Doesn't differential tuition cover the cost of the shop?</h3>
-              <p>Engineering undergraduate differential tuition is used to help pay for new equipment as well as equipment maintenance and repair. Differential tuition has made it possible to increase the number of equipment pieces in the shop, keep equipment current and allow for increased access to Shop equipment for students. It does not cover consumables used in the shop.</p>
+            <h3>Doesn't differential tuition cover the cost of the shop?</h3>
+              <p>Engineering undergraduate differential tuition is used to help pay for new equipment as well as equipment maintenance and repair. Differential tuition has made it possible to increase the number of equipment pieces in the shop, keep equipment current and allow for increased access to Shop equipment for students. Differential tuition is also used to pay for shop staff. It does not however, cover consumables used in the shop.</p>
 
-              <h3>Why does funded research have to pay more than non-research?</h3>
+            <h3>Why does funded research have to pay more than non-research?</h3>
               <p>Funded research pays more because the shop is partially funded with differential tuition which is only paid by undergraduates.</p>
 
-              <h3>Can I pay the fee for multiple fee periods all at once?</h3>
+            <h3>Can I pay the fee for multiple fee periods all at once?</h3>
               <p>Unfortunately, that is not possible at this time.</p>
 
-              <h3>Can I pay for a future fee period</h3>
+            <h3>Can I pay for a future fee period</h3>
               <p>Unfortunately, that is not possible at this time.</p>
 
-              <h3>Do I have to pay the whole fee even though I'm only using it for one short project?</h3>
+            <h3>Do I have to pay the whole fee even though I'm only using it for one short project?</h3>
               <p>Yes.</p>
 
-              <h3>I'm right at the edge of a fee period. Can I pay now and have it apply to the next fee period?</h3>
+            <h3>I'm right at the edge of a fee period. Can I pay now and have it apply to the next fee period?</h3>
               <p>Unfortunately, no. The month overlap at the start and end of each period is supposed to reduce the chances of this happening. However, if you are outside of the one month overlap but need shop access for both the current period and the next one, you will have to pay for both.</p>
 
-              <h3>Since this fee covers materials used in the shop, I don't have to pay for broken or lost equipment then?</h3>
+            <h3>Since this fee covers materials used in the shop, I don't have to pay for broken or lost equipment then?</h3>
               <p>No, if you damage expensive shop equipment you will still be liable for it's replacement. Some broken tooling may be covered by the fee. This is at the discretion of shop staff.</p>
 
-              <h3>My question wasn't answered in the FAQ. Whom do I contact?</h3>
+            <h3>My question wasn't answered in the FAQ. Whom do I contact?</h3>
               <p>Our apologies, please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a> with your question.</p>
 
             </div>
