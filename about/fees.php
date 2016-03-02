b35cd45afd9a0c210ccdf64111e137870a9fb483
Charles Allhands <charles.allhands@wisc.edu>
2016-03-02 16:26:42 -0600

    Update wording and research fee payment method

--- about/fees.php
+++ about/fees.php
@@ -40,15 +40,16 @@
 
               <h1>Charges &amp; Fees</h1>
 
-              <h2><a name="materialsfee"></a>Shop Materials Fee</h2>
+              <h2><a name="materialsfee"></a>Materials Fee</h2>
 
               <p>All shop users (note exceptions listed below) are required to pay a biannual materials fee of $50 to use the Student Shop (i.e. $100 per user, per year). The first period extends from December through June and the second extends from June through December (allowing a one month overlap at the start and end of each fee period).</p>
               <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables, such as copper, stainless steel, sheet metal, etc. must be purchased as needed.</p>
+              <p>Please log into EMU to <a href="https://emu.engr.wisc.edu/emu">pay the materials fee</a>.</p>
 
               <h3>Exceptions</h3>
               <ul>
                 <li>All faculty and University staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies) are exempt from the materials fee.</li>
-               <li>Teaching assistants for courses that use the shop may be entitled to have the fee waived. At this time that requires the professor of the course for which they act as a teaching assistant to <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a> with the teaching assistant's name and a means of identification (e.g., a campus ID number or NetID).</li>
+               <li>Teaching assistants for courses that use the shop may be entitled to have the fee waived. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</li>
               <li>Those working on funded research are required to pay the research fee (see below) rather than the materials fee.</li>
               <li>Individuals with a permit may check out hand tools (not including tooling, machine tools, or electric or pneumatic power tools) without paying the materials fee.</li>
               <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the fee (nor even a permit).</li></ul>
@@ -56,6 +57,7 @@
               <h2><a name="researchfee"></a>Research Fee</h2>
               <p>Individuals working on funded research are required to pay a biannual research fee of $100 to use the Student Shop (i.e. $200 per user, per year). The first period extends from December through June and the second extends from June through December (allowing a one month overlap at the start and end of each fee period).</p>
               <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables such as copper, stainless steel, sheet metal, etc. must be purchased as needed. This fee also covers repair, maintenance, and upgrading of shop equipment.</p>
+               <p>Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">pay the research fee</a>.</p>
 
               <h3>Exceptions</h3>
               <ul>
@@ -64,27 +66,28 @@
               <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the fee (nor even a permit).</li></ul>
 
               <h2><a name="feefaq"></a>Frequently Asked Questions</h2>
-              <h3>How do I pay the fee?</h3>
+              <h3>How do I pay the materials fee?</h3>
               <p>To pay the materials fee, <a href="https://emu.engr.wisc.edu/emu">log into EMU</a> and click the button at the top of the screen that says "Pay Fee Now". You will need a credit card to pay. A debit card set up to work as a credit card will also work.</p>
-            <p>To pay the research fee, have your professor or principal investigator (PI) <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include your name, a means of identification (e.g., your campus ID number or your NetID), and a University funding account to charge. There is no online payment method available at this time.</p>
+              <h3>How do I pay the research fee?</h3>
+            <p>Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">pay the research fee</a>.</p>
 
             <h3>I tried to pay online with a credit card but the system rejected my card. How do I pay?</h3>
             <p>Unfortunately, online payment via credit card is the only way to pay the materials fee. Most likely you entered incorrect information (this commonly occurs when using autofill). If you don't think that's the case your only option is to use a different credit card (maybe you could use a friend's or parent's card and reimburse them?).</p>
 
-            <h3>Can I pay with cash, Wiscard account, check, bitcoin, etc.?</h3>
+            <h3>Can I pay with cash, check, Wiscard account, bitcoin, etc.?</h3>
               <p>Unfortunately the only accepted method of payment for the materials fee is a credit card. We are exploring other options but they are not available at this time. The research fee can only be paid with a University funding account.</p>
 
             <h3>Can I pay with a ProCard?</h3>
               <p>Please consult with your departmental financial person to verify that this is an acceptable use of the <a href="http://www.bussvc.wisc.edu/acct/purchcd/">purchasing card</a>. The College prefers fees be paid via departmental transfer rather than by Procard to reduce unnecessary transfer fees.</p>
 
             <h3>I'm a student working on a funded research project in the shop. Which fee do I pay?</h3>
-              <p>Funded research must pay the research fee to work in the shop.</p>
+              <p>Funded research must <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">pay the research fee</a> to work in the shop.</p>
 
            <h3>I paid the materials fee but now I work on a funded research project. Does my research group need to pay the research fee as well?</h3>
               <p>Individuals working in the shop on funded research must pay the research fee even if they already paid the materials fee. If the materials fee was mistakenly paid when the research fee was intended please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a></p>
 
               <h3>Can I get my fee waived?</h3>
-                <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. They must have the professor of the course <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a> to request the fee be waived. The professor must include the name of the teaching assistant, a means of identifying them (e.g., Campus ID or NetID), and the course they are working for.</p>
+                <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</p>
 
             <h3>Can I be reimbursed by my department?</h3>
               <p>That is a departmental decision; however, you should not pay the fee yourself and then ask to be reimbursed. Speak with your department's financial person or your professor first and find out how they would like you to pay.</p>
@@ -96,8 +99,8 @@
               <p>No, services provided by the shop where you have to pay for the work being done do not require payment of the materials or research fee. If the work is being done free of charge either the material or research fee applies.</p>
 
             <h3>Can I pay on behalf of a student working for me?</h3>
-              <p>Yes, if you are paying the research fee <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#103;&#101;&#110;&#101;&#46;&#109;&#97;&#115;&#116;&#101;&#114;&#115;&#64;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email Gene Masters</a>. Include the student's name, a means of identification (e.g., their campus ID number or NetID), and a University funding account to charge.</p>
-              <p>You can request the fee be waived for teaching assistants if they are working for a course that uses the shop. Email Gene Masters and include the name of the teaching assistant, a means of identification (such as their Campus ID or NetID), and the course they are working for.</p>
+              <p>Yes, please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">pay the research fee</a>.</p>
+              <p>You can request the fee be waived for teaching assistants if they are working for a course that uses the shop. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</p>
               <p>If you are paying a student's materials fee you will need them to log into <a href="https://emu.engr.wisc.edu/emu">EMU</a> and click the "Pay Fee Now" button and then enter the credit card information for them.</p>
 
             <h3>One of my students paid the fee before I was able to have the fee waived. What can I do?</h3>
