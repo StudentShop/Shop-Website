4abe3e982189b51ca249c0f0f4135f56ecfcb805
Charles Allhands <charles.allhands@wisc.edu>
2016-06-08 10:11:42 -0500

    Clarify who doesn't pay material/research fees

--- about/fees.php
+++ about/fees.php
@@ -42,29 +42,29 @@
 
               <h2><a name="materialsfee"></a>Materials Fee</h2>
 
-              <p>All shop users (note exceptions listed below) are required to pay a biannual materials fee of $50 to use the Student Shop (i.e. $100 per user, per year). The first period extends from December through June and the second extends from June through December (allowing a one month overlap at the start and end of each fee period).</p>
+              <p>All shop users (note exceptions listed below) are required to pay a biannual materials fee of $50 to use the Student Shop (i.e. $100 per user, per year). The first period extends from December 1<sup>st</sup> through June 30<sup>th</sup> and the second extends from June 1<sup>st</sup> through December 31<sup>st</sup> (allowing a one month overlap at the start and end of each fee period).</p>
               <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables, such as copper, stainless steel, sheet metal, etc. must be purchased as needed.</p>
               <p>Please log into EMU to <a href="https://emu.engr.wisc.edu/emu">pay the materials fee</a>.</p>
 
               <h3>Exceptions</h3>
               <ul>
-                <li>All faculty and University staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies) are exempt from the materials fee.</li>
+                <li>The following employee classes are exempt from paying the research fee: Faculty (FA), Limited (LI), Classified Project (CJ), Academic Staff (AS), Univ Staff Ongoing Emplymnt (CP), Univ Staff Temporary Emplymnt (CL), Classified Permanent (cp), Univ Staff Project - FT Finite (CJ), OT Pensions (OT3), Ad Hoc Prg Spec/Corr Tutor (OT1), Zero Dollar Conc/Teach Supv (OT2), Classified LTE (CL), and Pre-College Intern (OT6).</li>
                 <li>Teaching assistants for courses that use the shop may be entitled to have the fee waived. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</li>
-                <li>Individuals employed by the College of Engineering to work on instructional projects (i.e., projects to support a course) are exempt from the materials fee.</li>
+                <li>Individuals employed by the College of Engineering to work on instructional projects (i.e., projects to support a course) are exempt from the materials fee. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</li>
                 <li>Those working on funded research are required to pay the research fee (see below) rather than the materials fee.</li>
                 <li>Individuals with a permit may check out hand tools (not including tooling, machine tools, or electric or pneumatic power tools) without paying the materials fee.</li>
-                <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the fee (nor even a permit).</li></ul>
+                <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the materials fee (nor even a permit).</li></ul>
 
               <h2><a name="researchfee"></a>Research Fee</h2>
-              <p>Individuals working on funded research are required to pay a biannual research fee of $100 to use the Student Shop (i.e. $200 per user, per year). The first period extends from December through June and the second extends from June through December (allowing a one month overlap at the start and end of each fee period).</p>
+              <p>Individuals working on funded research are required to pay a biannual research fee of $100 to use the Student Shop (i.e. $200 per user, per year). The first period extends from December 1<sup>st</sup> through June 30<sup>th</sup> and the second extends from June 1<sup>st</sup> through December 31<sup>st</sup> (allowing a one month overlap at the start and end of each fee period).</p>
               <p>This fee covers the cost of standard stocked consumables such as nuts, bolts, basic hardware, etc. Stockroom materials other than consumables such as copper, stainless steel, sheet metal, etc. must be purchased as needed. This fee also covers repair, maintenance, and upgrading of shop equipment.</p>
                <p>Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">pay the research fee</a>.</p>
 
               <h3>Exceptions</h3>
               <ul>
-                <li>All faculty and University staff including academic, classified permanent, classified LTE, classified project, and limited (but excluding student hourlies and some honorary assoc/fellows) are exempt from the research fee.</li>
-              <li>Individuals with a permit may check out hand tools (not including tooling, machine tools, or electric or pneumatic power tools) without paying the materials fee.</li>
-              <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the fee (nor even a permit).</li></ul>
+                <li>The following employee classes are exempt from paying the research fee: Faculty (FA), Limited (LI), Classified Project (CJ), Academic Staff (AS), Univ Staff Ongoing Emplymnt (CP), Univ Staff Temporary Emplymnt (CL), Classified Permanent (cp), Univ Staff Project - FT Finite (CJ), OT Pensions (OT3), Ad Hoc Prg Spec/Corr Tutor (OT1), Zero Dollar Conc/Teach Supv (OT2), Classified LTE (CL), and Pre-College Intern (OT6).</li>
+              <li>Individuals with a permit may check out hand tools (not including tooling, machine tools, or electric or pneumatic power tools) without paying the research fee.</li>
+              <li>Enlisting shop services for hire (<a href="/capabilities/3dprint/index.php">3d printing</a>, <a href="/capabilities/instrshop/index.php">Instrument Shop</a>, etc.) does not require paying the research fee (nor even a permit).</li></ul>
 
               <h2><a name="feefaq"></a>Frequently Asked Questions</h2>
               <h3>How do I pay the materials fee?</h3>
@@ -84,11 +84,14 @@
             <h3>I'm a student working on a funded research project in the shop. Which fee do I pay?</h3>
               <p>Funded research must <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">pay the research fee</a> to work in the shop.</p>
 
+            <h3>I'm emeritus faculty. Do I need to pay a fee?</h3>
+               <p>No, you are academic staff.</p>
+
+            <h3>I'm an honorary assoc/fellow. Do I need to pay a fee?</h3>
+              <p>Possibly, this is decided on a case by case basis (by the <a href="http://www.engr.wisc.edu/leadership.html">office of the Dean</a>). Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
+
            <h3>I paid the materials fee but now I work on a funded research project. Does my research group need to pay the research fee as well?</h3>
               <p>Individuals working in the shop on funded research must pay the research fee even if they already paid the materials fee. If the materials fee was mistakenly paid when the research fee was intended please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a></p>
-
-              <h3>Can I get my fee waived?</h3>
-                <p>The only individuals who can have their fee waived are teaching assistants working for a course that requires the shop. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</p>
 
             <h3>Can I be reimbursed by my department?</h3>
               <p>That is a departmental decision; however, you should not pay the fee yourself and then ask to be reimbursed. Speak with your department's financial person or your professor first and find out how they would like you to pay.</p>
@@ -104,14 +107,8 @@
               <p>You can request the fee be waived for teaching assistants if they are working for a course that uses the shop. Please use this form to <a href="https://emu.engr.wisc.edu/student_shop_fee/enter_student.php">request the fee be waived</a>.</p>
               <p>If you are paying a student's materials fee you will need them to log into <a href="https://emu.engr.wisc.edu/emu">EMU</a> and click the "Pay Fee Now" button and then enter the credit card information for them.</p>
 
-            <h3>One of my students paid the fee before I was able to have the fee waived. What can I do?</h3>
+            <h3>One of my students paid the materials fee before I was able to have the fee waived. What can I do?</h3>
               <p>Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
-
-            <h3>I'm emeritus faculty. Do I need to pay a fee?</h3>
-               <p>No, you are academic staff.</p>
-
-            <h3>I'm an honorary assoc/fellow. Do I need to pay a fee?</h3>
-              <p>Possibly, this is decided on a case by case basis (by the <a href="http://www.engr.wisc.edu/leadership.html">office of the Dean</a>). Please <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#116;&#117;&#100;&#101;&#110;&#116;&#115;&#104;&#111;&#112;&#64;&#101;&#110;&#103;&#114;&#46;&#119;&#105;&#115;&#99;&#46;&#101;&#100;&#117;">email the Student Shop</a>.</p>
 
             <h3>I just want training. Shouldn't I be exempt from the fee since I'm not using any materials?</h3>
               <p>No, nearly all training in the shop makes use of materials.</p>
